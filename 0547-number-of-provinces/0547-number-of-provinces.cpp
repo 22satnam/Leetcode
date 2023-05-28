9651b438cae045551934b4d30c61b281d1cff0c2
Satnam Singh <95736517+22satnam@users.noreply.github.com>
2023-05-28 18:36:57 +0530

    Time: 29 ms (42.17%), Space: 15 MB (28.42%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,45 +1,43 @@
 class Solution {
-public:
-
-void dfs(int node,vector<int> adj[],vector<int> &vis)
-{
-    vis[node] = 1;
-    for(auto it: adj[node])
-    {
-        if(!vis[it])
-         {
-              dfs(it,adj,vis);
-         }     
+    
+    private: 
+    // dfs traversal function 
+    void dfs(int node, vector<int> adjLs[], vector<int>&vis) {
+        // mark the more as visited
+        vis[node] = 1; 
+        for(auto it: adjLs[node]) {
+            if(!vis[it]) {
+                dfs(it, adjLs, vis); 
+            }
+        }
     }
-}
- 
-
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-
-        int n = isConnected.size();
-        vector<int> adj[n];
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(isConnected[i][j] == 1 && i!=j){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+        int V = isConnected.size();
+        vector<int> adjLs[V]; 
+        
+        // to change adjacency matrix to list 
+        for(int i = 0;i<V;i++) {
+            for(int j = 0;j<V;j++) {
+                // self nodes are not considered
+                if(isConnected[i][j] == 1 && i != j) {
+                    adjLs[i].push_back(j); 
+                    adjLs[j].push_back(i); 
                 }
             }
         }
-    vector<int> vis(n,0);
-    int count = 0;
-
-    for(int i = 0; i < n; i++){
-        if(!vis[i]){
-            count++;
-            dfs(i,adj,vis);
+        vector<int> vis(V,0);
+        int cnt = 0; 
+        for(int i = 0;i<V;i++) {
+            // if the node is not visited
+            if(!vis[i]) {
+                // counter to count the number of provinces 
+                cnt++;
+               dfs(i, adjLs, vis); 
+            }
         }
-    }
-    return count;
+        return cnt; 
         
     }
+    
 };
-
-
- 
